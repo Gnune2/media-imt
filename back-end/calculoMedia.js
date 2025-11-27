6116bdb20cbdefeb6885b3e6cbbf12047d2ed1d3
Gnune2 <226509660+Gnune2@users.noreply.github.com>
2025-11-27 15:18:49 -0300

    feat: novo script para calcular média, novas css e nova logo.

---
+++ back-end/calculoMedia.js
@@ -0,0 +1,23 @@
+const p1_nota = document.getElementById("p1");
+const p2_nota  = document.getElementById("p2");
+const t1_nota  = document.getElementById("t1");
+const t2_nota  = document.getElementById("t2");
+const botaoEnviar = document.getElementById("botaoEnviar");
+const resultadoDisplay = document.getElementById("resultadoDisplay");
+
+function enviarNotas() {
+    let p1 = parseFloat(p1_nota.value) || 0;
+    let p2 = parseFloat(p2_nota.value) || 0;
+    let t1 = parseFloat(t1_nota.value) || 0;
+    let t2 = parseFloat(t2_nota.value) || 0;
+ 
+    let notaFinal = 0.6 * ((p1 + p2)/2) + 0.4 * ((t1+t2)/2);
+
+    let situacao = notaFinal >= 6?"aprovado":"reprovado";
+
+    let mensagem = `Nota Final: ${notaFinal} - Situação: ${situacao}.`
+
+    resultadoDisplay.textContent = mensagem;
+}
+
+botaoEnviar.addEventListener("click", enviarNotas);
